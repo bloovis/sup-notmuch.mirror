822bb435be7f7196cf824425ca36d9a9e983bbb5
marka@pobox.com <marka@pobox.com>
2019-04-28 20:58:29 +0000

    When saving a sent message, use "notmuch insert" instead directly writing to the maildir.
    FossilOrigin-Name: ee593a81e74cb248a5f38b62e59e94a9aeeb6842d62d18ac0f8cf2e26a7911d2

--- lib/sup/sent.rb
+++ lib/sup/sent.rb
@@ -19,30 +19,16 @@ class SentManager
     stored = false
     ::Thread.new do
       debug "store the sent message"
-      new_fn = new_maildir_basefn + ':2,S'
-      Dir.chdir(@dir) do |d|
-	tmp_path = File.join(@dir, 'tmp', new_fn)
-	new_path = File.join(@dir, 'new', new_fn)
-	begin
-	  sleep 2 if File.stat(tmp_path)
-
-	  File.stat(tmp_path)
-	rescue Errno::ENOENT #this is what we want.
-	  begin
-	    File.open(tmp_path, 'wb') do |f|
-	      yield f #provide a writable interface for the caller
-	      f.fsync
-	    end
-
-	    File.safe_link tmp_path, new_path
-	    stored = true
-	  ensure
-	    File.unlink tmp_path if File.exist? tmp_path
-	  end
-	end #rescue Errno...
-      end #Dir.chdir
-      Notmuch.poll
-      PollManager.poll
+      pipe = IO.popen("notmuch insert --create-folder --folder=sent", "w:UTF-8")
+      if pipe
+	yield pipe
+	pipe.close
+	stored = true
+        Notmuch.poll
+        PollManager.poll
+      else
+	debug "Unable to store message!"
+      end
     end #Thread.new
     stored
   end
